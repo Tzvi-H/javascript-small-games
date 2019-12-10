6c7d3fe15cc0225696747000b0ef8cd62765242d
Tzvi-H <16guitar@gmail.com>
2019-12-10 11:54:36 +0800

    completed calculator and pseudocode lessons, also added .gitignore for node_module (readline-sync)

---
+++ lesson_2/pseudo_code.js
@@ -0,0 +1,42 @@
+// write out pseudocode (both casual and formal) that does the following:
+
+// a function that returns the sum of two numbers
+// a function that takes an array of strings, and returns a string that is all those strings concatenated together
+// a function that takes an array of integers, and returns a new array with every other element
+
+/*
+
+// a function that returns the sum of two numbers
+
+Given two numbers
+
+Add the sum of both numbers to a variable result
+
+Return the reusult
+
+
+// a function that takes an array of strings, and returns a string that is all those strings concatenated together
+
+Given an array of strings
+
+Create an empty result string
+Iterate over the input array
+- for each iteration
+  - append the current value to the string variable
+
+Return the string variable
+
+// a function that takes an array of integers, and returns a new array with every other element
+
+Given an array of integers
+
+Create an empty result array
+Create an index variable set to 1
+- While the index is less than the input array length
+    - get the value from the input array at the current index
+    - push the value to the result array
+    - increment index by 2
+
+Return the result array    
+
+*/
