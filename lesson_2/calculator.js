8927b4fa0e8d6bde806b1dbf0d7fb35896db3a49
Tzvi-H <16guitar@gmail.com>
2019-12-10 15:42:51 +0800

    completed walk-through refactoring calculator

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,34 +1,48 @@
-// ask user to enter number1
-// ask user to enter number2
-// ask user to enter operation
-// convert inputs to numbers
-// based on what the operation is, operate
-// on both numbers
-// print the result
-
 const readline = require('readline-sync');
 
-console.log('Welcome to the Calculator!');
-
-console.log("What's the first number?");
-let number1 = readline.question();
-
-console.log("What's the second number?");
-let number2 = readline.question();
-
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
-let operation = readline.question();
-
-let output;
-
-if (operation == '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation == '2') {
-  output = Number(number1) - Number(number2);
-} else if (operation == '3') {
-  output = Number(number1) * Number(number2);
-} else if (operation == '4') {
-  output = Number(number1) / Number(number2);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
-console.log(`The result is ${output}`);
+function invalidNumber(number) {
+  return number.trimStart() === '' || isNaN(Number(number));
+}
+
+prompt('Welcome to the Calculator!');
+
+prompt("What's the first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt(`'${number1}' is an invalid number. Please enter a valid number.`);
+  number1 = readline.question();
+}
+
+prompt("What's the second number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt(`'${number2}' is an invalid number. Please enter a valid number.`);
+  number2 = readline.question();
+}
+
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3, or 4');
+  operation = readline.question();
+}
+
+let output;
+switch (operation) {
+  case '1': output = Number(number1) + Number(number2);
+  break;
+  case '2': output = Number(number1) - Number(number2);
+  break;
+  case '3': output = Number(number1) * Number(number2);
+  break;
+  case '4': output = Number(number1) / Number(number2);
+}
+
+prompt(`The result is ${output}`);
