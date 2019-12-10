2b316207684cb3bf6fd3c0b3ebbc7a1d80257c28
Tzvi-H <16guitar@gmail.com>
2019-12-10 17:39:56 +0800

    calculator, added multi language feature

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,38 +1,43 @@
-const messages = require('./calculator_messages.json');
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
+const LANGUAGE = 'iw'; 
 
 function prompt(message) {
   console.log(`=> ${message}`);
+}
+
+function messages(message, lang='en') {
+  return MESSAGES[lang][message];
 }
 
 function invalidNumber(number) {
   return number.trimStart() === '' || isNaN(Number(number));
 }
 
-prompt(messages.welcome);
+prompt(messages('welcome'));
 
 while (true) {
-  prompt(messages.number1);
+  prompt(messages('number1', LANGUAGE));
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt(messages.invalidNumber);
+    prompt(messages('invalidNumber', LANGUAGE));
     number1 = readline.question();
   }
 
-  prompt(messages.number2);
+  prompt(messages('number2', LANGUAGE));
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt(messages.invalidNumber);
+    prompt(messages('invalidNumber', LANGUAGE));
     number2 = readline.question();
   }
 
-  prompt(messages.operations);
+  prompt(messages('operations', LANGUAGE));
   let operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(messages.invalidOperation);
+    prompt(messages('invalidOperation', LANGUAGE));
     operation = readline.question();
   }
 
@@ -47,9 +52,9 @@ while (true) {
     case '4': output = Number(number1) / Number(number2);
   }
 
-  prompt(`The result is ${output}`);
+  prompt(`The result is ${output}`, LANGUAGE);
 
-  prompt(messages.goAgain);
+  prompt(messages('goAgain', LANGUAGE));
   let answer = readline.question();
   if (answer[0].toLowerCase() !== 'y') break;
 }
