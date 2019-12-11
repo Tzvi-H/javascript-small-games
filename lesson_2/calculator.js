8cfe5b871c08e7eff1d23a319e898a62b46efa2b
Tzvi-H <16guitar@gmail.com>
2019-12-11 18:38:46 +0800

    moved operation validation into switch

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -35,22 +35,29 @@ while (true) {
 
   prompt(messages('operations'));
   let operation = READLINE.question();
-
-  while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(messages('invalidOperation'));
-    operation = READLINE.question();
-  }
-
   let output;
-  switch (operation) {
-    case '1': output = Number(number1) + Number(number2);
-              break;
-    case '2': output = Number(number1) - Number(number2);
-              break;
-    case '3': output = Number(number1) * Number(number2);
-              break;
-    case '4': output = Number(number1) / Number(number2);
-              break;
+
+  while (true) {
+    let invalidInput;
+
+    switch (operation) {
+      case '1': output = Number(number1) + Number(number2);
+                break;
+      case '2': output = Number(number1) - Number(number2);
+                break;
+      case '3': output = Number(number1) * Number(number2);
+                break;
+      case '4': output = Number(number1) / Number(number2);
+                break;
+      default : invalidInput = true;
+    }
+
+    if (invalidInput === true) {
+      prompt(messages('invalidOperation'));
+      operation = READLINE.question();
+    } else {
+      break;
+    }
   }
 
   prompt(`The result is ${output}`);
