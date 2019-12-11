1548812fdcd3425f21ccba256f7807970be79375
Tzvi-H <16guitar@gmail.com>
2019-12-11 10:24:08 +0800

    update variable names

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,44 +1,44 @@
-const readline = require('readline-sync');
+const READLINE = require('readline-sync');
 const MESSAGES = require('./calculator_messages.json');
-const LANGUAGE = 'iw';
+const LANGUAGE = 'en'; // 'en' for English, 'iw' for Hebrew
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function messages(message, lang = 'en') {
+function messages(message, lang = LANGUAGE) {
   return MESSAGES[lang][message];
 }
 
 function invalidNumber(number) {
-  return number.trimStart() === '' || isNaN(Number(number));
+  return Number.isNaN(Number(number)) || number.trimStart() === '';
 }
 
 prompt(messages('welcome'));
 
 while (true) {
-  prompt(messages('number1', LANGUAGE));
-  let number1 = readline.question();
+  prompt(messages('number1Prompt'));
+  let number1 = READLINE.question();
 
   while (invalidNumber(number1)) {
-    prompt(messages('invalidNumber', LANGUAGE));
-    number1 = readline.question();
+    prompt(messages('invalidNumber'));
+    number1 = READLINE.question();
   }
 
-  prompt(messages('number2', LANGUAGE));
-  let number2 = readline.question();
+  prompt(messages('number2Prompt'));
+  let number2 = READLINE.question();
 
   while (invalidNumber(number2)) {
-    prompt(messages('invalidNumber', LANGUAGE));
-    number2 = readline.question();
+    prompt(messages('invalidNumber'));
+    number2 = READLINE.question();
   }
 
-  prompt(messages('operations', LANGUAGE));
-  let operation = readline.question();
+  prompt(messages('operations'));
+  let operation = READLINE.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(messages('invalidOperation', LANGUAGE));
-    operation = readline.question();
+    prompt(messages('invalidOperation'));
+    operation = READLINE.question();
   }
 
   let output;
@@ -52,9 +52,9 @@ while (true) {
     case '4': output = Number(number1) / Number(number2);
   }
 
-  prompt(`The result is ${output}`, LANGUAGE);
+  prompt(`The result is ${output}`);
 
-  prompt(messages('goAgain', LANGUAGE));
-  let answer = readline.question();
+  prompt(messages('goAgain'));
+  let answer = READLINE.question();
   if (answer[0].toLowerCase() !== 'y') break;
 }
