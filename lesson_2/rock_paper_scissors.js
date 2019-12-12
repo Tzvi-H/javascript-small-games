2b7b0c8dc3c85843f54e71a9c0107d33289d1dc2
Tzvi-H <16guitar@gmail.com>
2019-12-12 17:24:03 +0800

    added lizard spock to game, added shortened input

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,43 +1,80 @@
 const READLINE = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
 
-let prompt = message => console.log(`=> ${message}`);
+function lineBreak() {
+  console.log();
+}
 
-let displayWinner = (userChoice, computerChoice) => {
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function displayRules() {
+  for (let move in WINNING_COMBOS) {
+    console.log(`${move} wins over: ${WINNING_COMBOS[move].join(' and ')}`);
+  }
+}
+
+function displayChoices() {
+  VALID_CHOICES.forEach((choice, index) => console.log(`${index + 1}: ${choice}`));
+}
+
+function invalidNumberChoice(numberChoice) {
+  return (numberChoice <= 0) ||
+         (numberChoice > VALID_CHOICES.length) ||
+         (!Number.isInteger(numberChoice));
+}
+
+function displayWinner(userChoice, computerChoice) {
   prompt(`You chose ${userChoice}, computer chose ${computerChoice}`);
 
-  if ((userChoice === 'rock' && computerChoice === 'scissors') ||
-      (userChoice === 'paper' && computerChoice === 'rock') ||
-      (userChoice === 'scissors' && computerChoice === 'paper')) {
+  if (WINNING_COMBOS[userChoice].includes(computerChoice)) {
       prompt('You win!');
-  } else if ((userChoice === 'rock' && computerChoice === 'paper') ||
-            (userChoice === 'paper' && computerChoice === 'scissors') ||
-            (userChoice === 'scissors' && computerChoice === 'rock')) {
+  } else if (WINNING_COMBOS[computerChoice].includes(userChoice)) {
     prompt('Computer wins!');
   } else {
     prompt("It's a tie");
   }
-};
+}
 
-let response;
+prompt(`Welcome to ${VALID_CHOICES.join(' ')}.`);
+lineBreak();
+displayRules();
+
+// loop exits when goAgainResponse matches
+// the right criteria in the while statement
+let goAgainResponse;
 do {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let userChoice = READLINE.question().toLowerCase();
-  while (!VALID_CHOICES.includes(userChoice)) {
-    prompt(`INVALID. Choose again (${VALID_CHOICES.join(', ')})`);
-    userChoice = READLINE.question().toLowerCase();
+  lineBreak();
+  prompt('Enter a number');
+  displayChoices();
+
+  let numberChoice = Number(READLINE.question());
+  while (invalidNumberChoice(numberChoice)) {
+    prompt('Please enter a valid number');
+    displayChoices();
+    numberChoice = Number(READLINE.question());
   }
+  let userChoice = VALID_CHOICES[numberChoice - 1];
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
   displayWinner(userChoice, computerChoice);
 
+  lineBreak();
+
   prompt('Do you want to play again? (yes/y) (no/n)');
-  response = READLINE.question().toLowerCase();
-  while (!['yes', 'y', 'no', 'n'].includes(response)) {
-    prompt(`'${response}' is an invalid input. Try again (yes/y) (no/n)`);
-    response = READLINE.question().toLowerCase();
+  goAgainResponse = READLINE.question().toLowerCase();
+  while (!['yes', 'y', 'no', 'n'].includes(goAgainResponse)) {
+    prompt(`'${goAgainResponse}' is an invalid input. Try again (yes/y) (no/n)`);
+    goAgainResponse = READLINE.question().toLowerCase();
   }
-  if (['no', 'n'].includes(response)) break;
-} while (['yes', 'y'].includes(response));
+} while (['yes', 'y'].includes(goAgainResponse));
